652c78127ee3c05983bd3f9a4c28358493f67257
MichaelMKKrause <44951724+MichaelMKKrause@users.noreply.github.com>
2024-05-15 15:55:54 +0200

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -163,9 +163,19 @@ class TestProductRoutes(TestCase):
         response = self.client.post(BASE_URL, data={}, content_type="plain/text")
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    #
-    # ADD YOUR TEST CASES HERE
-    #
+    def test_get_product(self):
+        """It should Get a single Product"""
+        # get the id of a product
+        test_product = self._create_products(1)[0]
+        # make a self.client.get request to the API endpoint and store the result in the variable named response
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        print(f"{BASE_URL}/{test_product.id}")
+        # assert that the resp.status_code is status.HTTP_200_OK
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # get the data from resp.get_json()
+        data = response.get_json()
+        # assert that data["name"] equals the test_product.name
+        self.assertEqual(data["name"], test_product.name)
 
     ######################################################################
     # Utility functions
